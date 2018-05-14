931448caa6bff33345bff4ab94eabd31ac84e262
Matti Cone <mcone@uoregon.edu>
2018-05-14 13:44:30 -0700

    added functions for erc20 tokens

---
+++ test/config/knownAssets.js
@@ -0,0 +1,504 @@
+module.exports = {
+    "BTC": {
+        "name": "Bitcoin",
+        "symbol": "BTC",
+        "image": "https://shapeshift.io/images/coins/bitcoin.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/bitcoin.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "FIRST": {
+        "name": "FirstBlood",
+        "symbol": "1ST",
+        "image": "https://shapeshift.io/images/coins/firstblood.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/firstblood.png",
+        "type": "erc20",
+        "contractAddress": "0xaf30d2a7e90d7dc361c8c4585e9bb7d2f6f15bc7"
+    },
+    "ANT": {
+        "name": "Aragon",
+        "symbol": "ANT",
+        "image": "https://shapeshift.io/images/coins/aragon.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/aragon.png",
+        "type": "erc20",
+        "contractAddress": "0x960b236A07cf122663c4303350609A66A7B288C0"
+    },
+    "BNT": {
+        "name": "Bancor",
+        "symbol": "BNT",
+        "image": "https://shapeshift.io/images/coins/bancor.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/bancor.png",
+        "type": "erc20",
+        "contractAddress": "0x1f573d6fb3f13d689ff844b4ce37794d79a7ff1c"
+    },
+    "BCH": {
+        "name": "Bitcoin Cash",
+        "symbol": "BCH",
+        "image": "https://shapeshift.io/images/coins/bitcoincash.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/bitcoincash.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "BTG": {
+        "name": "Bitcoin Gold",
+        "symbol": "BTG",
+        "image": "https://shapeshift.io/images/coins/bitcoingold.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/bitcoingold.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "BLK": {
+        "name": "Blackcoin",
+        "symbol": "BLK",
+        "image": "https://shapeshift.io/images/coins/blackcoin.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/blackcoin.png",
+        "type": "erc20",
+        "contractAddress": "0x9d09a8bb92f5b9dd10c562e58b8dd3661e8ea39e"
+    },
+    "CVC": {
+        "name": "Civic",
+        "symbol": "CVC",
+        "image": "https://shapeshift.io/images/coins/civic.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/civic.png",
+        "type": "erc20",
+        "contractAddress": "0x41e5560054824ea6b0732e656e3ad64e20e94e45"
+    },
+    "CLAM": {
+        "name": "Clams",
+        "symbol": "CLAM",
+        "image": "https://shapeshift.io/images/coins/clams.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/clams.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "DASH": {
+        "name": "Dash",
+        "symbol": "DASH",
+        "image": "https://shapeshift.io/images/coins/dash.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/dash.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "DCR": {
+        "name": "Decred",
+        "symbol": "DCR",
+        "image": "https://shapeshift.io/images/coins/decred.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/decred.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "DGB": {
+        "name": "DigiByte",
+        "symbol": "DGB",
+        "image": "https://shapeshift.io/images/coins/digibyte.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/digibyte.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "DNT": {
+        "name": "district0x",
+        "symbol": "DNT",
+        "image": "https://shapeshift.io/images/coins/district0x.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/district0x.png",
+        "type": "erc20",
+        "contractAddress": "0x0abdace70d3790235af448c88547603b945604ea"
+    },
+    "DOGE": {
+        "name": "Dogecoin",
+        "symbol": "DOGE",
+        "image": "https://shapeshift.io/images/coins/dogecoin.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/dogecoin.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "ETH": {
+        "name": "Ether",
+        "symbol": "ETH",
+        "image": "https://shapeshift.io/images/coins/ether.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/ether.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "ETC": {
+        "name": "Ether Classic",
+        "symbol": "ETC",
+        "image": "https://shapeshift.io/images/coins/etherclassic.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/etherclassic.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "FCT": {
+        "name": "Factoids",
+        "symbol": "FCT",
+        "image": "https://shapeshift.io/images/coins/factoids.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/factoids.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "GAME": {
+        "name": "GameCredits",
+        "symbol": "GAME",
+        "image": "https://shapeshift.io/images/coins/game.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/game.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "GNO": {
+        "name": "Gnosis",
+        "symbol": "GNO",
+        "image": "https://shapeshift.io/images/coins/gnosis.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/gnosis.png",
+        "type": "erc20",
+        "contractAddress": "0x6810e776880c02933d47db1b9fc05908e5386b96"
+    },
+    "GNT": {
+        "name": "Golem",
+        "symbol": "GNT",
+        "image": "https://shapeshift.io/images/coins/golem.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/golem.png",
+        "type": "erc20",
+        "contractAddress": "0xa74476443119A942dE498590Fe1f2454d7D4aC0d"
+    },
+    "GUP": {
+        "name": "Matchpool",
+        "symbol": "GUP",
+        "image": "https://shapeshift.io/images/coins/matchpool.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/matchpool.png",
+        "type": "erc20",
+        "contractAddress": "0xf7b098298f7c69fc14610bf71d5e02c60792894c"
+    },
+    "KMD": {
+        "name": "Komodo",
+        "symbol": "KMD",
+        "image": "https://shapeshift.io/images/coins/komodo.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/komodo.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "LBC": {
+        "name": "LBRY Credits",
+        "symbol": "LBC",
+        "image": "https://shapeshift.io/images/coins/lbry.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/lbry.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "LSK": {
+        "name": "Lisk",
+        "symbol": "LSK",
+        "image": "https://shapeshift.io/images/coins/lisk.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/lisk.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "LTC": {
+        "name": "Litecoin",
+        "symbol": "LTC",
+        "image": "https://shapeshift.io/images/coins/litecoin.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/litecoin.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "MAID": {
+        "name": "Maidsafe",
+        "symbol": "MAID",
+        "image": "https://shapeshift.io/images/coins/maidsafe.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/maidsafe.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "MONA": {
+        "name": "Monacoin",
+        "symbol": "MONA",
+        "image": "https://shapeshift.io/images/coins/monacoin.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/monacoin.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "MSC": {
+        "name": "Omni",
+        "symbol": "MSC",
+        "image": "https://shapeshift.io/images/coins/mastercoin.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/mastercoin.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "NEO": {
+        "name": "Neo",
+        "symbol": "NEO",
+        "image": "https://shapeshift.io/images/coins/neo.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/neo.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "NBT": {
+        "name": "Nubits",
+        "symbol": "NBT",
+        "image": "https://shapeshift.io/images/coins/nubits.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/nubits.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "NMC": {
+        "name": "Namecoin",
+        "symbol": "NMC",
+        "image": "https://shapeshift.io/images/coins/namecoin.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/namecoin.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "XEM": {
+        "name": "NEM",
+        "symbol": "XEM",
+        "specialReturn": false,
+        "specialOutgoing": true,
+        "specialIncoming": false,
+        "fieldName": "Message",
+        "fieldKey": "destTag",
+        "image": "https://shapeshift.io/images/coins/nem.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/nem.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "NMR": {
+        "name": "Numeraire",
+        "symbol": "NMR",
+        "image": "https://shapeshift.io/images/coins/numeraire.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/numeraire.png",
+        "type": "erc20",
+        "contractAddress": "0x1776e1f26f98b1a5df9cd347953a26dd3cb46671"
+    },
+    "NVC": {
+        "name": "Novacoin",
+        "symbol": "NVC",
+        "image": "https://shapeshift.io/images/coins/novacoin.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/novacoin.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "NXT": {
+        "name": "Nxt",
+        "symbol": "NXT",
+        "specialReturn": false,
+        "image": "https://shapeshift.io/images/coins/nxt.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/nxt.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "OMG": {
+        "name": "OmiseGo",
+        "symbol": "OMG",
+        "image": "https://shapeshift.io/images/coins/omisego.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/omisego.png",
+        "type": "erc20",
+        "contractAddress": "0xd26114cd6EE289AccF82350c8d8487fedB8A0C07"
+    },
+    "POT": {
+        "name": "Potcoin",
+        "symbol": "POT",
+        "image": "https://shapeshift.io/images/coins/potcoin.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/potcoin.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "PPC": {
+        "name": "Peercoin",
+        "symbol": "PPC",
+        "image": "https://shapeshift.io/images/coins/peercoin.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/peercoin.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "QTUM": {
+        "name": "Qtum",
+        "symbol": "QTUM",
+        "image": "https://shapeshift.io/images/coins/qtum.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/qtum.png",
+        "type": "token",
+        "contractAddress": "0x9a642d6b3368ddc662CA244bAdf32cDA716005BC"
+    },
+    "REP": {
+        "name": "Augur",
+        "symbol": "REP",
+        "image": "https://shapeshift.io/images/coins/augur.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/augur.png",
+        "type": "erc20",
+        "contractAddress": "0xe94327d07fc17907b4db788e5adf2ed424addff6"
+    },
+    "RDD": {
+        "name": "Reddcoin",
+        "symbol": "RDD",
+        "image": "https://shapeshift.io/images/coins/reddcoin.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/reddcoin.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "RCN": {
+        "name": "RCN",
+        "symbol": "RCN",
+        "image": "https://shapeshift.io/images/coins/rcn.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/rcn.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "RLC": {
+        "name": "iExec",
+        "symbol": "RLC",
+        "image": "https://shapeshift.io/images/coins/iexec.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/iexec.png",
+        "type": "erc20",
+        "contractAddress": "0x607F4C5BB672230e8672085532f7e901544a7375"
+    },
+    "SALT": {
+        "name": "Salt",
+        "symbol": "SALT",
+        "image": "https://shapeshift.io/images/coins/salt.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/salt.png",
+        "type": "erc20",
+        "contractAddress": "0x4156D3342D5c385a87D264F90653733592000581"
+    },
+    "SC": {
+        "name": "Siacoin",
+        "symbol": "SC",
+        "image": "https://shapeshift.io/images/coins/siacoin.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/siacoin.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "SNT": {
+        "name": "Status",
+        "symbol": "SNT",
+        "image": "https://shapeshift.io/images/coins/status.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/status.png",
+        "type": "erc20",
+        "contractAddress": "0x744d70fdbe2ba4cf95131626614a1763df805b9e"
+    },
+    "STORJ": {
+        "name": "Storj",
+        "symbol": "STORJ",
+        "image": "https://shapeshift.io/images/coins/storj.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/storj.png",
+        "type": "erc20",
+        "contractAddress": "0xb64ef51c888972c908cfacf59b47c1afbc0ab8ac"
+    },
+    "START": {
+        "name": "Startcoin",
+        "symbol": "START",
+        "image": "https://shapeshift.io/images/coins/startcoin.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/startcoin.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "STEEM": {
+        "name": "Steem",
+        "symbol": "STEEM",
+        "specialReturn": false,
+        "specialOutgoing": true,
+        "specialIncoming": true,
+        "fieldName": "destTag",
+        "fieldKey": "destTag",
+        "image": "https://shapeshift.io/images/coins/steem.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/steem.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "SWT": {
+        "name": "Swarm City",
+        "symbol": "SWT",
+        "image": "https://shapeshift.io/images/coins/swarmcity.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/swarmcity.png",
+        "type": "erc20",
+        "contractAddress": "0xb9e7f8568e08d5659f5d29c4997173d84cdf2607"
+    },
+    "TRST": {
+        "name": "WeTrust",
+        "symbol": "TRST",
+        "image": "https://shapeshift.io/images/coins/wetrust.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/wetrust.png",
+        "type": "erc20",
+        "contractAddress": "0xcb94be6f13a1182e4a4b6140cb7bf2025d28e41b"
+    },
+    "USDT": {
+        "name": "Tether",
+        "symbol": "USDT",
+        "image": "https://shapeshift.io/images/coins/tether.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/tether.png",
+        "type": "token",
+        "contractAddress": "0xdac17f958d2ee523a2206206994597c13d831ec7"
+    },
+    "VOX": {
+        "name": "Voxels",
+        "symbol": "VOX",
+        "image": "https://shapeshift.io/images/coins/voxels.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/voxels.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "VRC": {
+        "name": "Vericoin",
+        "symbol": "VRC",
+        "image": "https://shapeshift.io/images/coins/vericoin.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/vericoin.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "VTC": {
+        "name": "Vertcoin",
+        "symbol": "VTC",
+        "image": "https://shapeshift.io/images/coins/vertcoin.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/vertcoin.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "WINGS": {
+        "name": "Wings",
+        "symbol": "WINGS",
+        "image": "https://shapeshift.io/images/coins/wings.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/wings.png",
+        "type": "erc20",
+        "contractAddress": "0x667088b212ce3d06a1b553a7221E1fD19000d9aF"
+    },
+    "XMR": {
+        "name": "Monero",
+        "symbol": "XMR",
+        "specialReturn": false,
+        "specialOutgoing": true,
+        "specialIncoming": true,
+        "fieldName": "Payment Id",
+        "qrName": "tx_payment_id",
+        "fieldKey": "paymentId",
+        "image": "https://shapeshift.io/images/coins/monero.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/monero.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "XRP": {
+        "name": "Ripple",
+        "symbol": "XRP",
+        "specialReturn": false,
+        "specialOutgoing": true,
+        "specialIncoming": true,
+        "fieldName": "Destination Tag",
+        "fieldKey": "destTag",
+        "image": "https://shapeshift.io/images/coins/ripple.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/ripple.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "ZEC": {
+        "name": "Zcash",
+        "symbol": "ZEC",
+        "image": "https://shapeshift.io/images/coins/zcash.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/zcash.png",
+        "type": "",
+        "contractAddress": ""
+    },
+    "ZRX": {
+        "name": "0x",
+        "symbol": "ZRX",
+        "image": "https://shapeshift.io/images/coins/0x.png",
+        "imageSmall": "https://shapeshift.io/images/coins-sm/0x.png",
+        "type": "erc20",
+        "contractAddress": "0xe41d2489571d322189246dafa5ebde1f4699f498"
+    }
+}
---
+++ test/erc20TokenQuery_test.js
@@ -0,0 +1,114 @@
+/*eslint-env node, mocha, es6 */
+process.env.NODE_ENV = 'test';
+const chai = require('chai');
+const assert = chai.assert;
+const knownAssets = require('./config/knownAssets.js');
+const erc20 = require('../src/erc20_token_query');
+
+suite('erc20 token query', function() {
+    this.timeout(3000);
+    test('fails if no address is provided', (done) => {
+        erc20.getTokenContractInfo()
+        .then(() => {
+          done(new Error('Expected method to reject.'))
+        })
+        .catch((err) => {
+          assert.isDefined(err);
+          done();
+        })
+        .catch(done);
+    });
+    test('api will retreive ERC20 token information for wallet address', async() => {
+        const ethAddress = '0x3028beecaf98393d144876bc533dbdfac2116739'
+        const result = await erc20.getAddressInfo(ethAddress);
+        assert.equal(result.address, ethAddress);
+    });
+    test('api functions for Firstblood token', async() => {
+        const result = await erc20.getTokenContractInfo(knownAssets.FIRST.contractAddress);
+        assert.equal(result.name, 'Firstblood');
+    });
+    test('api functions for Aragon token', async() => {
+        const result = await erc20.getTokenContractInfo(knownAssets.ANT.contractAddress);
+        assert.equal(result.name, 'Aragon Network Token');
+    });
+    test('api functions for Bancor token', async() => {
+        const result = await erc20.getTokenContractInfo(knownAssets.BNT.contractAddress);
+        assert.equal(result.name, 'Bancor Network Token');
+    });
+    test('api functions for Blackcoin token', async() => {
+        const result = await erc20.getTokenContractInfo(knownAssets.BLK.contractAddress);
+        assert.equal(result.name, 'BLACKcoin');
+    });
+    test('api functions for Civic token', async() => {
+        const result = await erc20.getTokenContractInfo(knownAssets.CVC.contractAddress);
+        assert.equal(result.name, 'Civic');
+    });
+    test('api functions for district0x token', async() => {
+        const result = await erc20.getTokenContractInfo(knownAssets.DNT.contractAddress);
+        assert.equal(result.name, 'district0x Network Token');
+    });
+    test('api functions for Gnosis token', async() => {
+        const result = await erc20.getTokenContractInfo(knownAssets.GNO.contractAddress);
+        assert.equal(result.name, 'Gnosis Token');
+    });
+    test('api functions for Golem token', async() => {
+        const result = await erc20.getTokenContractInfo(knownAssets.GNT.contractAddress);
+        assert.equal(result.name, 'Golem Network Token');
+    });
+    test('api functions for Matchpool token', async() => {
+        const result = await erc20.getTokenContractInfo(knownAssets.GUP.contractAddress);
+        assert.equal(result.name, 'Guppy');
+    });
+    test('api functions for Numeraire token', async() => {
+        const result = await erc20.getTokenContractInfo(knownAssets.NMR.contractAddress);
+        assert.equal(result.name, 'Numeraire');
+    });
+    test('api functions for OmiseGo token', async() => {
+        const result = await erc20.getTokenContractInfo(knownAssets.OMG.contractAddress);
+        assert.equal(result.name, 'OMGToken');
+    });
+    test('api functions for Qtum token', async() => {
+        const result = await erc20.getTokenContractInfo(knownAssets.QTUM.contractAddress);
+        assert.equal(result.name, 'Qtum');
+    });
+    test('api functions for Augur token', async() => {
+        const result = await erc20.getTokenContractInfo(knownAssets.REP.contractAddress);
+        assert.equal(result.name, 'Reputation');
+    });
+    test('api functions for iExec token', async() => {
+        const result = await erc20.getTokenContractInfo(knownAssets.RLC.contractAddress);
+        assert.equal(result.name, 'iEx.ec Network Token');
+    });
+    test('api functions for Salt token', async() => {
+        const result = await erc20.getTokenContractInfo(knownAssets.SALT.contractAddress);
+        assert.equal(result.name, 'Salt');
+    });
+    test('api functions for Status token', async() => {
+        const result = await erc20.getTokenContractInfo(knownAssets.SNT.contractAddress);
+        assert.equal(result.name, 'Status Network');
+    });
+    test('api functions for Storj token', async() => {
+        const result = await erc20.getTokenContractInfo(knownAssets.STORJ.contractAddress);
+        assert.equal(result.name, 'StorjToken');
+    });
+    test('api functions for Swarm City token', async() => {
+        const result = await erc20.getTokenContractInfo(knownAssets.SWT.contractAddress);
+        assert.equal(result.name, 'Swarm City Token');
+    });
+    test('api functions for WeTrust token', async() => {
+        const result = await erc20.getTokenContractInfo(knownAssets.TRST.contractAddress);
+        assert.equal(result.name, 'Trustcoin');
+    });
+    test('api functions for Tether token', async() => {
+        const result = await erc20.getTokenContractInfo(knownAssets.USDT.contractAddress);
+        assert.equal(result.name, 'Tether USD');
+    });
+    test('api functions for Wings token', async() => {
+        const result = await erc20.getTokenContractInfo(knownAssets.WINGS.contractAddress);
+        assert.equal(result.name, 'WINGS');
+    });
+    test('api functions for 0x token', async() => {
+        const result = await erc20.getTokenContractInfo(knownAssets.ZRX.contractAddress);
+        assert.equal(result.name, '0x Protocol Token');
+    });
+});
