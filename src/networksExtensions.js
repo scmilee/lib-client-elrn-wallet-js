74f0828574e720a967a5671aa6b191d082cbc657
Cole Albon <calbon@cbtnuggets.com>
2018-05-26 06:38:50 -0700

    add multiple altcoins

--- src/networksExtensions.js
+++ src/networksExtensions.js
@@ -1,165 +1,305 @@
 // list of coins from Ian Coleman's bip39 cross referenced with shapeshift
 // https://raw.githubusercontent.com/iancoleman/bip39/master/src/js/bitcoinjs-extensions.js
 module.exports = {
-  bitcoin: {
-    messagePrefix: '\x18Bitcoin Signed Message:\n',
-    bech32: 'bc',
-    bip32: {
-      public: 0x0488b21e,
-      private: 0x0488ade4
-    },
-    pubKeyHash: 0x00,
-    scriptHash: 0x05,
-    wif: 0x80,
-    p2wpkh: {
-      baseNetwork: "bitcoin",
-      messagePrefix: '\x18Bitcoin Signed Message:\n',
-      bech32: 'bc',
-      bip32: {
-          public: 0x04b24746,
-          private: 0x04b2430c
-      },
-      pubKeyHash: 0x00,
-      scriptHash: 0x05,
-      wif: 0x80
-    },
-    p2wpkhInP2sh: {
-        baseNetwork: "bitcoin",
+    bitcoin: {
         messagePrefix: '\x18Bitcoin Signed Message:\n',
         bech32: 'bc',
         bip32: {
-            public: 0x049d7cb2,
-            private: 0x049d7878
+            public: 0x0488b21e,
+            private: 0x0488ade4
         },
         pubKeyHash: 0x00,
         scriptHash: 0x05,
-        wif: 0x80
-    }
-  },
-  testnet: {
-    messagePrefix: '\x18Bitcoin Signed Message:\n',
-    bech32: 'tb',
-    bip32: {
-      public: 0x043587cf,
-      private: 0x04358394
+        wif: 0x80,
+        p2wpkh: {
+            baseNetwork: "bitcoin",
+            messagePrefix: '\x18Bitcoin Signed Message:\n',
+            bech32: 'bc',
+            bip32: {
+                public: 0x04b24746,
+                private: 0x04b2430c
+            },
+            pubKeyHash: 0x00,
+            scriptHash: 0x05,
+            wif: 0x80
+        },
+        p2wpkhInP2sh: {
+            baseNetwork: "bitcoin",
+            messagePrefix: '\x18Bitcoin Signed Message:\n',
+            bech32: 'bc',
+            bip32: {
+                public: 0x049d7cb2,
+                private: 0x049d7878
+            },
+            pubKeyHash: 0x00,
+            scriptHash: 0x05,
+            wif: 0x80
+        }
     },
-    pubKeyHash: 0x6f,
-    scriptHash: 0xc4,
-    wif: 0xef,
-    p2wpkh: {
-        baseNetwork: "testnet",
+    testnet: {
         messagePrefix: '\x18Bitcoin Signed Message:\n',
         bech32: 'tb',
         bip32: {
-            public: 0x045f1cf6,
-            private: 0x045f18bc
+            public: 0x043587cf,
+            private: 0x04358394
         },
         pubKeyHash: 0x6f,
         scriptHash: 0xc4,
-        wif: 0xef
-    },
-    p2wpkhInP2sh: {
-        baseNetwork: "testnet",
-        messagePrefix: '\x18Bitcoin Signed Message:\n',
-        bech32: 'tb',
-        bip32: {
-            public: 0x044a5262,
-            private: 0x044a4e28
+        wif: 0xef,
+        p2wpkh: {
+            baseNetwork: "testnet",
+            messagePrefix: '\x18Bitcoin Signed Message:\n',
+            bech32: 'tb',
+            bip32: {
+                public: 0x045f1cf6,
+                private: 0x045f18bc
+            },
+            pubKeyHash: 0x6f,
+            scriptHash: 0xc4,
+            wif: 0xef
         },
-        pubKeyHash: 0x6f,
-        scriptHash: 0xc4,
-        wif: 0xef
-    }
-  },
-  litecoin: {
-    messagePrefix: '\x19Litecoin Signed Message:\n',
-    bip32: {
-      public: 0x019da462,
-      private: 0x019d9cfe
+        p2wpkhInP2sh: {
+            baseNetwork: "testnet",
+            messagePrefix: '\x18Bitcoin Signed Message:\n',
+            bech32: 'tb',
+            bip32: {
+                public: 0x044a5262,
+                private: 0x044a4e28
+            },
+            pubKeyHash: 0x6f,
+            scriptHash: 0xc4,
+            wif: 0xef
+        }
     },
-    pubKeyHash: 0x30,
-    scriptHash: 0x32,
-    wif: 0xb0,
-    p2wpkhInP2sh: {
-        baseNetwork: "litecoin",
+    litecoin: {
         messagePrefix: '\x19Litecoin Signed Message:\n',
         bip32: {
-            public: 0x01b26ef6,
-            private: 0x01b26792
+            public: 0x019da462,
+            private: 0x019d9cfe
         },
         pubKeyHash: 0x30,
         scriptHash: 0x32,
+        wif: 0xb0,
+        p2wpkhInP2sh: {
+            baseNetwork: "litecoin",
+            messagePrefix: '\x19Litecoin Signed Message:\n',
+            bip32: {
+                public: 0x01b26ef6,
+                private: 0x01b26792
+            },
+            pubKeyHash: 0x30,
+            scriptHash: 0x32,
+            wif: 0xb0
+        }
+    },
+    zcash: {
+        messagePrefix: '\x18Zcash Signed Message:\n',
+        bip32: {
+            public: 0x0488B21E,
+            private: 0x0488ADE4,
+        },
+        pubKeyHash: 0x1CB8,
+        scriptHash: 0x1CBD,
+        wif: 0x80
+    },
+    dogecoin: {
+        messagePrefix: '\x19Dogecoin Signed Message:\n',
+        bip32: {
+            public: 0x02facafd,
+            private: 0x02fac398
+        },
+        pubKeyHash: 0x1e,
+        scriptHash: 0x16,
+        wif: 0x9e
+    },
+    bgold: {
+        messagePrefix: 'unused',
+        bip32: {
+            public: 0x0488b21e,
+            private: 0x0488ade4
+        },
+        pubKeyHash: 38,
+        scriptHash: 23,
+        wif: 128
+    },
+    blackcoin: {
+        messagePrefix: '\x18BlackCoin Signed Message:\n',
+        bip32: {
+            public: 0x02CFBEDE,
+            private: 0x02CFBF60
+        },
+        pubKeyHash: 0x19,
+        scriptHash: 0x55,
+        wif: 0x99
+    },
+    clam: {
+        messagePrefix: 'unused',
+        bip32: {
+            public: 0xa8c26d64,
+            private: 0xa8c17826
+        },
+        pubKeyHash: 0x89,
+        scriptHash: 0x00, // TODO set this correctly
+        wif: 0x85
+    },
+    dash: {
+        messagePrefix: 'unused',
+        bip32: {
+            public: 0x0488b21e,
+            private: 0x0488ade4
+        },
+        pubKeyHash: 0x4c,
+        scriptHash: 0x10,
+        wif: 0xcc
+    },
+    digibyte: {
+        messagePrefix: '\x18DigiByte Signed Message:\n',
+        bip32: {
+            public: 0x0488B21E,
+            private: 0x0488ADE4,
+        },
+        pubKeyHash: 0x1e,
+        scriptHash: 0x05,
+        wif: 0x80,
+    },
+    game: {
+        messagePrefix: 'unused',
+        bip32: {
+            public: 0x0488b21e,
+            private: 0x0488ade4
+        },
+        pubKeyHash: 0x26,
+        scriptHash: 0x05,
+        wif: 0xa6
+    },
+    monacoin: {
+        messagePrefix: '\x18Monacoin Signed Message:\n',
+        bip32: {
+            public: 0x0488b21e,
+            private: 0x0488ade4
+        },
+        pubKeyHash: 0x32,
+        scriptHash: 0x37,
         wif: 0xb0
+    },
+    omnicore: {
+        messagePrefix: '\x18Bitcoin Signed Message:\n',
+        bip32: {
+            public: 0x0488B21E,
+            private: 0x0488ADE4,
+        },
+        pubKeyHash: 0x00,
+        scriptHash: 0x05,
+        wif: 0x80,
+    },
+    neoscoin: {
+        messagePrefix: '\x18NeosCoin Signed Message:\n',
+        bip32: {
+            public: 0x0488B21E,
+            private: 0x0488ADE4,
+        },
+        pubKeyHash: 0x35,
+        scriptHash: 0x05,
+        wif: 0xb1,
+    },
+    nubits: {
+        messagePrefix: '\x18Nu Signed Message:\n',
+        bip32: {
+            public: 0x0488b21e,
+            private: 0x0488ade4
+        },
+        pubKeyHash: 0x19,
+        scriptHash: 0x1a,
+        wif: 0x96,
+    },
+    namecoin: {
+        messagePrefix: 'unused',
+        bip32: {
+            public: 0x0488b21e,
+            private: 0x0488ade4
+        },
+        pubKeyHash: 0x34,
+        scriptHash: 0x00, // TODO set this correctly
+        wif: 0x80
+    },
+    novacoin: {
+        messagePrefix: '\x18NovaCoin Signed Message:\n',
+        bip32: {
+            public: 0x0488b21e,
+            private: 0x0488ade4,
+        },
+        pubKeyHash: 0x08,
+        scriptHash: 0x14,
+        wif: 0x88,
+    },
+    potcoin: {
+        messagePrefix: '\x18Potcoin Signed Message:\n',
+        bip32: {
+            public: 0x0488b21e,
+            private: 0x0488ade4,
+        },
+        pubKeyHash: 0x37,
+        scriptHash: 0x05,
+        wif: 0xb7,
+    },
+    peercoin: {
+        messagePrefix: 'unused',
+        bip32: {
+            public: 0x0488b21e,
+            private: 0x0488ade4
+        },
+        pubKeyHash: 0x37,
+        scriptHash: 0x00, // TODO set this correctly
+        wif: 0xb7
+    },
+    reddcoin: {
+        messagePrefix: '\x18Reddcoin Signed Message:\n',
+        bip32: {
+            public: 0x0488B21E,
+            private: 0x0488ADE4,
+        },
+        pubKeyHash: 0x3d,
+        scriptHash: 0x05,
+        wif: 0xbd,
+    },
+    komodo: {
+        messagePrefix: '\x18Komodo Signed Message:\n',
+        bip32: {
+            public: 0x0488B21E,
+            private: 0x0488ADE4
+        },
+        pubKeyHash: 0x3c,
+        scriptHash: 0x55,
+        wif: 0xbc
+    },
+    lbry: {
+        messagePrefix: '\x18LBRYcrd Signed Message:\n',
+        bip32: {
+            public: 0x0488B21E,
+            private: 0x0488ADE4,
+        },
+        pubKeyHash: 0x55,
+        scriptHash: 0x7a,
+        wif: 0x1c,
+    },
+    revolutionvr: {
+        messagePrefix: '\x18Voxels Signed Message:\n',
+        bip32: {
+            public: 0x0488B21E,
+            private: 0x0488ADE4,
+        },
+        pubKeyHash: 0x46,
+        scriptHash: 0x05,
+        wif: 0xc6,
+    },
+    vertcoin: {
+        messagePrefix: '\x18Vertcoin Signed Message:\n',
+        bip32: {
+            public: 0x0488b21e,
+            private: 0x0488ade4,
+        },
+        pubKeyHash: 0x47,
+        scriptHash: 0x05,
+        wif: 0x80,
     }
-  },
-  zcash: {
-    messagePrefix: '\x18Zcash Signed Message:\n',
-    bip32: {
-        public: 0x0488B21E,
-        private: 0x0488ADE4,
-    },
-    pubKeyHash: 0x1CB8,
-    scriptHash: 0x1CBD,
-    wif: 0x80
-  },
-  dogecoin: {
-    messagePrefix: '\x19Dogecoin Signed Message:\n',
-    bip32: {
-      public: 0x02facafd,
-      private: 0x02fac398
-    },
-    pubKeyHash: 0x1e,
-    scriptHash: 0x16,
-    wif: 0x9e
-  },
-  bgold: {
-    messagePrefix: 'unused',
-      bip32: {
-        public: 0x0488b21e,
-        private: 0x0488ade4
-      },
-      pubKeyHash: 38,
-      scriptHash: 23,
-      wif: 128
-  },
-  blackcoin: {
-    messagePrefix: '\x18BlackCoin Signed Message:\n',
-    bip32: {
-      public: 0x02CFBEDE,
-      private: 0x02CFBF60
-    },
-    pubKeyHash: 0x19,
-    scriptHash: 0x55,
-    wif: 0x99
-  },
-  clam: {
-    messagePrefix: 'unused',
-    bip32: {
-      public: 0xa8c26d64,
-      private: 0xa8c17826
-    },
-    pubKeyHash: 0x89,
-    scriptHash: 0x00, // TODO set this correctly
-    wif: 0x85
-  },
-  dash: {
-    messagePrefix: 'unused',
-    bip32: {
-      public: 0x0488b21e,
-      private: 0x0488ade4
-    },
-    pubKeyHash: 0x4c,
-    scriptHash: 0x10,
-    wif: 0xcc
-  },
-  digibyte: {
-    messagePrefix: '\x18DigiByte Signed Message:\n',
-    bip32: {
-      public: 0x0488B21E,
-      private: 0x0488ADE4,
-    },
-    pubKeyHash: 0x1e,
-    scriptHash: 0x05,
-    wif: 0x80,
-  }
 }
--- test/seedToAddress_test.js
+++ test/seedToAddress_test.js
@@ -4,11 +4,7 @@ const chai = require('chai');
 const isBuffer = require('is-buffer')
 const assert = chai.assert;
 const bufferFrom = require('buffer-from');
-const bip44Coins = require('bip44-constants');
-//import seedToMnemonic from '../src/seedToMnemonic.js';
 import Elrn from '..';
-
-//'c88e288c7d4904aa7ac31e910cf2a0474dfc3f88b4fac7ab3a88dc5aaa8ffa0d0bd569e6a45e130048e4d0be5d2cf5d20d63496093f6560fd63eaf9a048832d9'
 
 suite('seedToAddress', function() {
     // test('seedToAddress returns a string', function() {
@@ -38,7 +34,7 @@ suite('seedToAddress', function() {
     //     .then((mnemonic) => assert.notEqual(true, true))
     //     .catch((error) => assert.equal(error.message, 'Expected BIP32 derivation path, got undefined'))
     // });
-    test('bitcoin address', function() {
+    test('bitcoin', function() {
         const config = require(__dirname + '/config/options.js');
         const elrnClient = new Elrn(config)
         //spring range position vocal foot tilt drastic diet tube song debris excite
@@ -50,7 +46,7 @@ suite('seedToAddress', function() {
             assert.equal(address, '164Cw1K93cYrme5s9VdCaPXLpTXE35RN9e')
         })
     });
-    test('litecoin address', function() {
+    test('litecoin', function() {
         const config = require(__dirname + '/config/options.js');
         const elrnClient = new Elrn(config)
         //spring range position vocal foot tilt drastic diet tube song debris excite
@@ -62,7 +58,7 @@ suite('seedToAddress', function() {
             assert.equal(address, 'LQarSkd7ewx4qW1Mf7m56JtiQXufS2Trdd')
         })
     });
-    test('dogecoin address', function() {
+    test('dogecoin', function() {
         const config = require(__dirname + '/config/options.js');
         const elrnClient = new Elrn(config)
         //spring range position vocal foot tilt drastic diet tube song debris excite
@@ -74,7 +70,7 @@ suite('seedToAddress', function() {
             assert.equal(address, 'D5MfWuxRhPpL6BoST1gHwkUL8sEqneitqo')
         })
     });
-    test('bitcoin gold address', function() {
+    test('bitcoin gold', function() {
         const config = require(__dirname + '/config/options.js');
         const elrnClient = new Elrn(config)
         //spring range position vocal foot tilt drastic diet tube song debris excite
@@ -86,7 +82,7 @@ suite('seedToAddress', function() {
             assert.equal(address, 'GLbFEsH33fEn7iQFPNSVW52kPTAUbCieNu')
         })
     });
-    test('blackcoin address', function() {
+    test('blackcoin', function() {
         const config = require(__dirname + '/config/options.js');
         const elrnClient = new Elrn(config)
         //spring range position vocal foot tilt drastic diet tube song debris excite
@@ -98,7 +94,7 @@ suite('seedToAddress', function() {
             assert.equal(address, 'BGducTAHrsowjjjmtgYaXr1St1hsK1PRXe')
         })
     });
-    test('clam address', function() {
+    test('clam', function() {
         const config = require(__dirname + '/config/options.js');
         const elrnClient = new Elrn(config)
         //spring range position vocal foot tilt drastic diet tube song debris excite
@@ -110,7 +106,7 @@ suite('seedToAddress', function() {
             assert.equal(address, 'xLqjAxG1wNHKtC9cMMbseHaFJKTNdXfKLS')
         })
     });
-    test('dash address', function() {
+    test('dash', function() {
         const config = require(__dirname + '/config/options.js');
         const elrnClient = new Elrn(config)
         //spring range position vocal foot tilt drastic diet tube song debris excite
@@ -122,7 +118,7 @@ suite('seedToAddress', function() {
             assert.equal(address, 'XfmnvKyrj6vrBAJy2eZ8f45HPt3xTB6X29')
         })
     });
-    test('digibyte address', function() {
+    test('digibyte', function() {
         const config = require(__dirname + '/config/options.js');
         const elrnClient = new Elrn(config)
         //spring range position vocal foot tilt drastic diet tube song debris excite
@@ -132,6 +128,150 @@ suite('seedToAddress', function() {
         .then((address) => {
             assert.equal(typeof address,  'string')
             assert.equal(address, 'DBWNN48Gez6rCsUBuEp4fsoYYbP5u98xV3')
+        })
+    });
+    test('game credits', function() {
+        const config = require(__dirname + '/config/options.js');
+        const elrnClient = new Elrn(config)
+        //spring range position vocal foot tilt drastic diet tube song debris excite
+        let seed = bufferFrom('c88e288c7d4904aa7ac31e910cf2a0474dfc3f88b4fac7ab3a88dc5aaa8ffa0d0bd569e6a45e130048e4d0be5d2cf5d20d63496093f6560fd63eaf9a048832d9', 'hex')
+        let derivePath = "m/44'/101'/0'/0/0"
+        return elrnClient.seedToAddress(seed, derivePath, 'game')
+        .then((address) => {
+            assert.equal(typeof address,  'string')
+            assert.equal(address, 'GQEwKc76g8yH5iG6V2sXamJg7xdWv3sksa')
+        })
+    });
+    test('monacoin', function() {
+        const config = require(__dirname + '/config/options.js');
+        const elrnClient = new Elrn(config)
+        //spring range position vocal foot tilt drastic diet tube song debris excite
+        let seed = bufferFrom('c88e288c7d4904aa7ac31e910cf2a0474dfc3f88b4fac7ab3a88dc5aaa8ffa0d0bd569e6a45e130048e4d0be5d2cf5d20d63496093f6560fd63eaf9a048832d9', 'hex')
+        let derivePath = "m/44'/22'/0'/0/0"
+        return elrnClient.seedToAddress(seed, derivePath, 'monacoin')
+        .then((address) => {
+            assert.equal(typeof address,  'string')
+            assert.equal(address, 'MFqAa7w8PVs4P13KZtFhwMo42k2fQVjDww')
+        })
+    });
+    test('omnicore', function() {
+        const config = require(__dirname + '/config/options.js');
+        const elrnClient = new Elrn(config)
+        //spring range position vocal foot tilt drastic diet tube song debris excite
+        let seed = bufferFrom('c88e288c7d4904aa7ac31e910cf2a0474dfc3f88b4fac7ab3a88dc5aaa8ffa0d0bd569e6a45e130048e4d0be5d2cf5d20d63496093f6560fd63eaf9a048832d9', 'hex')
+        let derivePath = "m/44'/200'/0'/0/0"
+        return elrnClient.seedToAddress(seed, derivePath, 'omnicore')
+        .then((address) => {
+            assert.equal(typeof address,  'string')
+            assert.equal(address, '1HKz8NAZ9DfE9mdTzA2fX1sLWVoPwnW5Y4')
+        })
+    });
+    test('neoscoin', function() {
+        const config = require(__dirname + '/config/options.js');
+        const elrnClient = new Elrn(config)
+        //spring range position vocal foot tilt drastic diet tube song debris excite
+        let seed = bufferFrom('c88e288c7d4904aa7ac31e910cf2a0474dfc3f88b4fac7ab3a88dc5aaa8ffa0d0bd569e6a45e130048e4d0be5d2cf5d20d63496093f6560fd63eaf9a048832d9', 'hex')
+        let derivePath = "m/44'/25'/0'/0/0"
+        return elrnClient.seedToAddress(seed, derivePath, 'neoscoin')
+        .then((address) => {
+            assert.equal(typeof address,  'string')
+            assert.equal(address, 'NhAiSRK6KS1yxqShU5aQ9vEUK3gxgCCL7c')
+        })
+    });
+    test('namecoin', function() {
+        const config = require(__dirname + '/config/options.js');
+        const elrnClient = new Elrn(config)
+        //spring range position vocal foot tilt drastic diet tube song debris excite
+        let seed = bufferFrom('c88e288c7d4904aa7ac31e910cf2a0474dfc3f88b4fac7ab3a88dc5aaa8ffa0d0bd569e6a45e130048e4d0be5d2cf5d20d63496093f6560fd63eaf9a048832d9', 'hex')
+        let derivePath = "m/44'/7'/0'/0/0"
+        return elrnClient.seedToAddress(seed, derivePath, 'namecoin')
+        .then((address) => {
+            assert.equal(typeof address,  'string')
+            assert.equal(address, 'N1ox4X8Yw1aU3XikB3W8tdTKCCuUA3QVAn')
+        })
+    });
+    test('novacoin', function() {
+        const config = require(__dirname + '/config/options.js');
+        const elrnClient = new Elrn(config)
+        //spring range position vocal foot tilt drastic diet tube song debris excite
+        let seed = bufferFrom('c88e288c7d4904aa7ac31e910cf2a0474dfc3f88b4fac7ab3a88dc5aaa8ffa0d0bd569e6a45e130048e4d0be5d2cf5d20d63496093f6560fd63eaf9a048832d9', 'hex')
+        let derivePath = "m/44'/50'/0'/0/0"
+        return elrnClient.seedToAddress(seed, derivePath, 'novacoin')
+        .then((address) => {
+            assert.equal(typeof address,  'string')
+            assert.equal(address, '4bsNwTfSnn13UbbWrg5dCv7dcUEBxYU4kv')
+        })
+    });
+    test('potcoin', function() {
+        const config = require(__dirname + '/config/options.js');
+        const elrnClient = new Elrn(config)
+        //spring range position vocal foot tilt drastic diet tube song debris excite
+        let seed = bufferFrom('c88e288c7d4904aa7ac31e910cf2a0474dfc3f88b4fac7ab3a88dc5aaa8ffa0d0bd569e6a45e130048e4d0be5d2cf5d20d63496093f6560fd63eaf9a048832d9', 'hex')
+        let derivePath = "m/44'/81'/0'/0/0"
+        return elrnClient.seedToAddress(seed, derivePath, 'potcoin')
+        .then((address) => {
+            assert.equal(typeof address,  'string')
+            assert.equal(address, 'PWoUwZ8qVazJzrXgWdznkqdLpQgsBqC4DX')
+        })
+    });
+    test('peercoin', function() {
+        const config = require(__dirname + '/config/options.js');
+        const elrnClient = new Elrn(config)
+        //spring range position vocal foot tilt drastic diet tube song debris excite
+        let seed = bufferFrom('c88e288c7d4904aa7ac31e910cf2a0474dfc3f88b4fac7ab3a88dc5aaa8ffa0d0bd569e6a45e130048e4d0be5d2cf5d20d63496093f6560fd63eaf9a048832d9', 'hex')
+        let derivePath = "m/44'/6'/0'/0/0"
+        return elrnClient.seedToAddress(seed, derivePath, 'peercoin')
+        .then((address) => {
+            assert.equal(typeof address,  'string')
+            assert.equal(address, 'PKhK5CQXSAu7DX5aZ1t4yAkMVa9uuTx7Qu')
+        })
+    });
+    test('komodo', function() {
+        const config = require(__dirname + '/config/options.js');
+        const elrnClient = new Elrn(config)
+        //spring range position vocal foot tilt drastic diet tube song debris excite
+        let seed = bufferFrom('c88e288c7d4904aa7ac31e910cf2a0474dfc3f88b4fac7ab3a88dc5aaa8ffa0d0bd569e6a45e130048e4d0be5d2cf5d20d63496093f6560fd63eaf9a048832d9', 'hex')
+        let derivePath = "m/44'/141'/0'/0/0"
+        return elrnClient.seedToAddress(seed, derivePath, 'komodo')
+        .then((address) => {
+            assert.equal(typeof address,  'string')
+            assert.equal(address, 'RJbGR69HvTwJtZFjTomK4gwEwZzAgpn39p')
+        })
+    });
+    test('lbry', function() {
+        const config = require(__dirname + '/config/options.js');
+        const elrnClient = new Elrn(config)
+        //spring range position vocal foot tilt drastic diet tube song debris excite
+        let seed = bufferFrom('c88e288c7d4904aa7ac31e910cf2a0474dfc3f88b4fac7ab3a88dc5aaa8ffa0d0bd569e6a45e130048e4d0be5d2cf5d20d63496093f6560fd63eaf9a048832d9', 'hex')
+        let derivePath = "m/44'/140'/0'/0/0"
+        return elrnClient.seedToAddress(seed, derivePath, 'lbry')
+        .then((address) => {
+            assert.equal(typeof address,  'string')
+            assert.equal(address, 'bR8wiWfVn8de8a7FyNhL4937VB5FLCFkBw')
+        })
+    });
+    test('revolutionvr', function() {
+        const config = require(__dirname + '/config/options.js');
+        const elrnClient = new Elrn(config)
+        //spring range position vocal foot tilt drastic diet tube song debris excite
+        let seed = bufferFrom('c88e288c7d4904aa7ac31e910cf2a0474dfc3f88b4fac7ab3a88dc5aaa8ffa0d0bd569e6a45e130048e4d0be5d2cf5d20d63496093f6560fd63eaf9a048832d9', 'hex')
+        let derivePath = "m/44'/129'/0'/0/0"
+        return elrnClient.seedToAddress(seed, derivePath, 'revolutionvr')
+        .then((address) => {
+            assert.equal(typeof address,  'string')
+            assert.equal(address, 'VVxWvKPyfQvmUxV1xc8wpgLtjAvsZuNtLH')
+        })
+    });
+    test('vertcoin', function() {
+        const config = require(__dirname + '/config/options.js');
+        const elrnClient = new Elrn(config)
+        //spring range position vocal foot tilt drastic diet tube song debris excite
+        let seed = bufferFrom('c88e288c7d4904aa7ac31e910cf2a0474dfc3f88b4fac7ab3a88dc5aaa8ffa0d0bd569e6a45e130048e4d0be5d2cf5d20d63496093f6560fd63eaf9a048832d9', 'hex')
+        let derivePath = "m/44'/28'/0'/0/0"
+        return elrnClient.seedToAddress(seed, derivePath, 'vertcoin')
+        .then((address) => {
+            assert.equal(typeof address,  'string')
+            assert.equal(address, 'Vn859fLnRi6y5zU5PsTzwQuEfc9PnUSUWJ')
         })
     });
 });
