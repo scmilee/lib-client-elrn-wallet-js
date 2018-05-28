c82533ce99428ec34a869d6e2b6ff7be5cb66651
Cole Albon <calbon@cbtnuggets.com>
2018-05-27 21:46:32 -0700

    add listNetworks.js

--- src/listNetworks.js
+++ src/listNetworks.js
@@ -1,7 +1,16 @@
-export default function createSeed(size=16) {
+var bitcoin = require('bitcoinjs-lib');
+var networksExtensions = require('./networksExtensions.js');
+
+export default function listNetworks() {
     return new Promise((resolve, reject) => {
         try {
-            resolve(bitcoin.networks);
+            bitcoin.networks = Object.assign(bitcoin.networks, networksExtensions)
+            let networksArray = []
+            Object.keys(bitcoin.networks).map((networkName) => {
+                const jsonl = `{${networkName}: ${bitcoin.networks[networkName]}}`
+                networksArray.push(jsonl);
+            })
+            resolve(networksArray);
         } catch (err) {
             reject(err);
         }
