37deabc0f331dc6a170dd63824538c040ad279f6
Matti Cone <mcone@uoregon.edu>
2018-05-30 13:44:20 -0700

    fixed linting

--- src/seedPrivateKeyToEthereumAddress.js
+++ src/seedPrivateKeyToEthereumAddress.js
@@ -9,24 +9,24 @@ var ethereum = require('ethereumjs-wallet');
 
 export default function seedPrivateKeyToEthereumAddress(key, path) {
   if (typeof key != 'string') {
-      return Promise.reject(new Error('must call seedPrivateKeyToEthereumAddress with a key'))
+      return Promise.reject(new Error('must call seedPrivateKeyToEthereumAddress with a key'));
   }
   if (typeof path != 'string') {
-      return Promise.reject(new Error('must call seedPrivateKeyToEthereumAddress with a path'))
+      return Promise.reject(new Error('must call seedPrivateKeyToEthereumAddress with a path'));
   }
   return new Promise((resolve,reject) => {
       try{
-        let splitPath = path.split('/');
-        const derivedPath = splitPath.splice(0,5).join('/');
-        const index = splitPath.join();
-        const hdWallet = hdkey.fromExtendedKey(key);
-        const derivedNode = hdWallet.derivePath(derivedPath)
-        const nodeChild = derivedNode.deriveChild(index);
-        const childWallet = nodeChild.getWallet();
-        const walletAddress= childWallet.getAddressString();
-        resolve(walletAddress)
+          let splitPath = path.split('/');
+          const derivedPath = splitPath.splice(0,5).join('/');
+          const index = splitPath.join();
+          const hdWallet = hdkey.fromExtendedKey(key);
+          const derivedNode = hdWallet.derivePath(derivedPath);
+          const nodeChild = derivedNode.deriveChild(index);
+          const childWallet = nodeChild.getWallet();
+          const walletAddress= childWallet.getAddressString();
+          resolve(walletAddress);
       } catch (err) {
-        reject(err)
+          reject(err);
       }
   })
 }
