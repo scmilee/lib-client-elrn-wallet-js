dff255a64470f05731f90f0a342b567353c7eb12
Matti Cone <mcone@uoregon.edu>
2018-05-30 13:17:01 -0700

    fixed linting

--- test/seedPrivateKeyToEthereumAddress_test.js
+++ test/seedPrivateKeyToEthereumAddress_test.js
@@ -12,23 +12,23 @@ suite('seedToEthereumAddress', () => {
     let path = null;
     const key ='xprv9s21ZrQH143K2T7SGZv2WsPQEh8rvr6jxx9KH99hoMXTHgxNAqjUYQYBF8GLBSmszjYGCAJw1rnbce2V3pNXwEC77FRqDt88RVSGh3qtfYR';
     test('Errors if no key is supplied', () => {
-      return elrnClient.seedPrivateKeyToEthereumAddress(null, path)
-      .then((result) => assert.notEqual(true, true))
-      .catch((error) => assert.equal(error.message, 'must call seedPrivateKeyToEthereumAddress with a key'))
-    })
+        return elrnClient.seedPrivateKeyToEthereumAddress(null, path)
+        .then((result) => assert.notEqual(true, true))
+        .catch((error) => assert.equal(error.message, 'must call seedPrivateKeyToEthereumAddress with a key'));
+    });
     test('Errors if no path is supplied', () => {
-      return elrnClient.seedPrivateKeyToEthereumAddress(key, path)
-      .then((result) => assert.notEqual(true, true))
-      .catch((error) => assert.equal(error.message, 'must call seedPrivateKeyToEthereumAddress with a path'))
-    })
+        return elrnClient.seedPrivateKeyToEthereumAddress(key, path)
+        .then((result) => assert.notEqual(true, true))
+        .catch((error) => assert.equal(error.message, 'must call seedPrivateKeyToEthereumAddress with a path'));
+    });
     test('seedToEthereumAddress returns a known value for index 0', () => {
         path = 'm/44\'/60\'/0\'/0/0';
         return elrnClient.seedPrivateKeyToEthereumAddress(key, path)
-        .then((ethereumAddress) => assert.equal(ethereumAddress, '0x7c2e172d59f11a308d1ceefa1f0cf91314021cc3'))
+        .then((ethereumAddress) => assert.equal(ethereumAddress, '0x7c2e172d59f11a308d1ceefa1f0cf91314021cc3'));
     });
     test('seedToEthereumAddress returns a known value for index 1', () => {
-        path = 'm/44\'/60\'/0\'/0/1'
+        path = 'm/44\'/60\'/0\'/0/1';
         return elrnClient.seedPrivateKeyToEthereumAddress(key, path)
-        .then((ethereumAddress) => assert.equal(ethereumAddress, '0x95c939159e446b16889920fbe400023f59799236'))
+        .then((ethereumAddress) => assert.equal(ethereumAddress, '0x95c939159e446b16889920fbe400023f59799236'));
     });
 });
