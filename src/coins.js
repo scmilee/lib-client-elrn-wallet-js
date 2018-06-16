8a55791e87bb495779b3502d79b61fda7068fd5b
Cole Albon <calbon@cbtnuggets.com>
2018-06-16 14:26:45 -0700

    fix bulk of erc20 address generation

--- src/coins.js
+++ src/coins.js
@@ -1,29 +1,60 @@
 const shapeshift = require('shapeshift.io')
-
-const erc20Extensions = require('./erc20Extensions.js')
 const extensions = require('./networksExtensions') // bitcoinjs
 
 // augment shapeshift coins results with known values for ETH tokens and bitcoinjs
 export default () => {
     return new Promise((resolve, reject) => {
         try {
-          
+
           shapeshift.coins((err, coins) => {
               if (err) {
                   reject(err);
               }
               let extendedCoins = {}
               Object.keys(coins).map((coin) => {
+                  // surpressed symbols
+                  if (coin == 'BCH') {
+                      return
+                  }
+                  if (coin == 'DCR') {
+                      return
+                  }
+                  if (coin == 'ETH') {
+                      return
+                  }
+                  if (coin == 'ETC') {
+                      return
+                  }
+                  if (coin == 'NMC') {
+                      return
+                  }
+                  if (coin == 'NXT') {
+                      return
+                  }
+                  if (coin == 'RDD') {
+                      return
+                  }
+                  if (coin == 'RCN') {
+                      return
+                  }
+                  if (coin == 'SC') {
+                      return
+                  }
+                  if (coins[coin].name == 'Augur') {
+                      return
+                  }
+                  if (coin == 'XMR') {
+                      return
+                  }
+                  if (coin == 'XRP') {
+                      return
+                  }
+                  if (coin == 'ZEC') {
+                      return
+                  }
                   extendedCoins[coin] = Object.assign(coins[coin]);
                   extendedCoins[coin].id = coin;
                   extendedCoins[coin].derivePath = "m/0'/0/0";
-                  Object.keys(erc20Extensions).map((erc20extension) => {
-                    if (erc20Extensions[erc20extension].symbol == coin) {
-                      extendedCoins[coin].id = erc20extension;
-                      extendedCoins[coin].derivePath = "m/44'/60'/0'/0/0";
-                      extendedCoins[coin].extension = Object.assign(erc20extensions[erc20extension]);
-                    }
-                  })
                   Object.keys(extensions).map((extension) => {
                     if (extensions[extension].symbol == coin) {
                       extendedCoins[coin].id = extension;
--- src/seedToAddress.js
+++ src/seedToAddress.js
@@ -1,15 +1,30 @@
 var bitcoin = require('bitcoinjs-lib')
+var hdkey = require('ethereumjs-wallet/hdkey')
 const isBuffer = require('is-buffer')
 
-export default function seedToAddress(seed, path, coin) {
+export default function seedToAddress(seed, path, coinExt) {
     if (isBuffer(seed) != true) Promise.reject(new Error('must call seedToAddress with a buffer (see createSeed.js)'))
     if (!path) Promise.reject(new Error('must call seedToAddress with a derive path'))
     return new Promise((resolve, reject) => {
         try {
-            var root = bitcoin.HDNode.fromSeedBuffer(seed, bitcoin.networks[coin]);
-            let child = root.derivePath(path)
-            var keyPair = child.keyPair;
-            resolve(keyPair.getAddress().toString());
+            if (coinExt.type == 'bitcoinjs') {
+                var root = bitcoin.HDNode.fromSeedBuffer(seed, coinExt);
+                let derivedNode = root.derivePath(path)
+                var keyPair = derivedNode.keyPair;
+                resolve(keyPair.getAddress().toString());
+            }
+            if (coinExt.type == 'erc20') {
+                let splitPath = path.split('/');
+                const derivedPath = splitPath.splice(0,5).join('/');
+                const index = splitPath.join();
+                const hdWallet = hdkey.fromMasterSeed(seed);
+                const derivedNode = hdWallet.derivePath(derivedPath);
+                const nodeChild = derivedNode.deriveChild(index);
+                const childWallet = nodeChild.getWallet();
+                const walletAddress= childWallet.getAddressString();
+                resolve(walletAddress);
+            }
+
         } catch (err) {
             reject(err);
         }
