4daa9edd855a2d70512004d684c1c8a1de022394
Cole Albon <calbon@cbtnuggets.com>
2018-06-21 11:17:01 -0700

    fix paths on integration test, add CBT community docs

--- test/integration/xPubToActivity_test.js
+++ test/integration/xPubToActivity_test.js
@@ -4,11 +4,11 @@ const chai = require('chai');
 const isBuffer = require('is-buffer')
 const assert = chai.assert;
 const bufferFrom = require('buffer-from');
-import Elrn from '..';
+import Elrn from '../..';
 
 suite('xPubToActivity', function() {
     test('xPubToActivity - bitcoin', function() {
-        const config = require(__dirname + '/config/options.js');
+        const config = require(__dirname + '/../config/options.js');
         const elrnClient = new Elrn(config)
         //spring range position vocal foot tilt drastic diet tube song debris excite
         let xPubString = 'xpub6CajDacdHWChWvVNMVDEmUQAmB68cGa5xibJspixagJBT88Mb2rFhrHX3qw4rVXb1XkZ6AhE3vM9TtowSzgBu5xCpquBYxYbvWof5N5fTWD'
