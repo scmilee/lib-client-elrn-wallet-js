d86354769975c98c096a22af0eba9644342d5679
Cole Albon <calbon@cbtnuggets.com>
2018-06-22 09:34:03 -0700

    Update xPubToActivity.js

--- src/xPubToActivity.js
+++ src/xPubToActivity.js
@@ -4,7 +4,7 @@ var memoize = require('memoizee');
 
 const slow_xPubToActivity = function(xPubString, network) {
   if(!xPubString) {return Promise.reject(new Error('called xPubToUnspentOutputs without providing address'))}
-  if(!xPubString) {return Promise.reject(new Error('called xPubToUnspentOutputs without providing network (example "bitcoin")'))}
+  if(!network) {return Promise.reject(new Error('called xPubToUnspentOutputs without providing network (example "bitcoin")'))}
   return new Promise((resolve, reject) => {
       try {
           switch (network) {
