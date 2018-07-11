3716b06266b7941107eba3f637edc91b96430eaf
scmilee <drudged@oregonstate.edu>
2018-07-11 14:49:22 -0700

    add a function that takes a Xpubkey and an array of address numbers and returns an array of private keys for all of those address indicies

---
+++ test/xPrivToCoinPrivKey_test.js
@@ -0,0 +1,21 @@
+/*eslint-env node, mocha, es6 */
+process.env.NODE_ENV = 'test';
+const chai = require('chai');
+const bufferFrom = require('buffer-from');
+const assert = chai.assert;
+import Elrn from '..';
+
+const config = require(__dirname + '/config/options.js');
+const elrnClient = new Elrn(config);
+
+suite('xPriv to Coin PrivKey', function() {
+    test('returns a coin address', function() {
+        const addressNumbers = [0,2];
+        //mnemonic is -> strategy worth garlic gallery gesture happy trash boil ceiling weasel happy patrol
+        //the extended account private key below is from the 0 account from the mnemonic above
+        const privKey = elrnClient.xPrivToCoinPrivKey( 'xprv9yEcdEMShowWaR4d4QG1kuhTDeQAe5arJtppZqknZUT8n81XehMjjC9EsNQuDUGtPULvm3E5s3qSkhXEL7WGKMJREMpSeeN1pdJTSK1qigj', addressNumbers)
+        
+        assert.equal(privKey[0], 'L3X8PYNGYpgvbhsVMyhLpUdaTHGpShnJjgKJnnnakzsgqbaWKnCf');
+        assert.equal(privKey[1], 'L2Snqii6ignMgN2LrhJ7WT6UqM4vqGDdWqCLr2qGmWsK75rNqk8g');
+    });
+});
