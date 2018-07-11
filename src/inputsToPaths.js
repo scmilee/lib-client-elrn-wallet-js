27d2c3d967c53c887d26ca15eeff207ba1ddd36f
scmilee <drudged@oregonstate.edu>
2018-07-11 14:32:11 -0700

    add a function that will take inputs from unsepentOutputsToInputs function, identify the derived path of the addresses in the inputs, and return the path addresses in an array.

---
+++ src/inputsToPaths.js
@@ -0,0 +1,25 @@
+var bitcoin = require('bitcoinjs-lib');
+
+export default (inputs, accountPubKey) => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cumulativePaths = [];
+      let newPath;
+      Object.keys(inputs).map((inputTransaction) => {
+        Object.keys(inputs[inputTransaction]).map((inputn) => {
+          //make sure the accounts pubkey is the same as this unspentoutputs
+          if (accountPubKey == inputs[inputTransaction][inputn].xpub.m) {
+            //set the path as our own, split and slice it to remove the m/0 baggage, then add it to the array of paths
+            newPath = inputs[inputTransaction][inputn].xpub.path 
+            let splitPath = newPath.split('/');
+            let coinAddressIndex = splitPath.slice(-1)[0]
+            cumulativePaths.push(coinAddressIndex);
+          }
+        })
+      })
+      resolve(cumulativePaths)
+    } catch(err) {
+      reject(err)
+    }
+  })
+}
