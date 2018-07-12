d8e8437714f2d095d26880f56dfbd757099b6744
scmilee <drudged@oregonstate.edu>
2018-07-12 14:15:34 -0700

    fix seed to root function to return a promise, to work well with the chain wreck

--- src/seedToRoot.js
+++ src/seedToRoot.js
@@ -2,12 +2,13 @@ var bitcoin = require('bitcoinjs-lib')
 var hdkey = require('ethereumjs-wallet/hdkey')
 const isBuffer = require('is-buffer')
 
-export default (seed, coinExt) => {
-    try {
-        const rootNode = bitcoin.HDNode.fromSeedBuffer(seed);
-        return rootNode; 
-      
-      } catch (err) {
-        console.log(err);
-     }
+export default (seed) => {
+    return new Promise((resolve, reject) => {
+        try {
+            const rootNode = bitcoin.HDNode.fromSeedBuffer(seed);
+        	resolve(rootNode); 
+        } catch (err) {
+            reject(err);
+        }
+    });
 };
