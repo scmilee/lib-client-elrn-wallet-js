ecbc9d7580c42ee7a551ded12ef83978bd36783c
scmilee <drudged@oregonstate.edu>
2018-07-17 10:35:08 -0700

    add broadcastTransaction function, that takes and unbuilt transaction. Builds it, converts it to hex and then broadcasts it to blockchains api for transactions

---
+++ src/broadcastTransaction.js
@@ -0,0 +1,25 @@
+const bitcoin = require('bitcoinjs-lib')
+const axios = require('axios')
+//takes an unsigned transaction and an array of privatekeys sourced from all of the unspent outputs
+export default (transaction) => {
+    return new Promise((resolve, reject) => {
+       try {
+        const hexxedTx = transaction.build().toHex();
+        const url = `https://www.blockchain.com/en/btc/pushtx?cors=true`
+
+        axios({
+          method: 'post',
+          url: url,
+          data: $.param({tx:transaction}),
+          headers:{
+            "Content-Type":"application/x-www-form-urlencoded"
+          }
+        })
+        .then(function(response) {
+            resolve(response.data);
+        })
+      	} catch (err) {
+        	reject(err);
+     	}
+    });
+}
