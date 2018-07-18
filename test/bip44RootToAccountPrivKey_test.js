4b4754c3ab9bd47ee4624d90a8174505975c9b0e
scmilee <drudged@oregonstate.edu>
2018-07-18 10:26:47 -0700

    fix locations of tests, and the names of masterRoot tests

--- test/bip44RootToAccountPrivKey_test.js
+++ test/bip44RootToAccountPrivKey_test.js
@@ -1,4 +1,4 @@
-/*eslint-env node, mocha, es6 */
+ /*eslint-env node, mocha, es6 */
 process.env.NODE_ENV = 'test';
 const chai = require('chai');
 const bufferFrom = require('buffer-from');
@@ -9,21 +9,22 @@ import Elrn from '..';
 const config = require(__dirname + '/config/options.js');
 const elrnClient = new Elrn(config);
 
-suite('masterRootToAccountPubKey', function() {
-    test('returns a xpub key to a account', function() {
+suite('bip44RootToAccountPrivKey', function() {
+    test('returns a xpriv key to a account', function() {
         
         const derivedPathStr = "m/44'/0'/0'/0/0";
         //mnemonic--> salad ability key forget baby visit dress whip spray arrange fun orphan tip spin mansion
         let seed = bufferFrom('0aa3860185456253d2bbdb7fb5675d039e9873f5b249ba778f0b79148738320c4b0a6b4cdca8067bc8c1e70aa0953d87fbd9e49686777fb7fa348f2af4a06e88', 'hex');
-        const rootNode = elrnClient.seedToRoot(seed);
+        let rootnode;
+
         const accountNumber = 3;
-      
         elrnClient.seedToRoot(seed)
             .then((rootNode) => {
-                return elrnClient.masterRootToAccountPubKey(rootNode, derivedPathStr , accountNumber)
+                return elrnClient.bip44RootToAccountPrivKey(rootNode, derivedPathStr , accountNumber)
                 .then((address) => {
-                        assert.equal(address, 'xpub6BymPu2L82c7RMjMMhLPR3z8XNPsgoZLWX6v94zUnkRB6cxiPyqWmHUVHoVJKp3Zv3V3XcZ6t7gATaARueGb1LgRcVczwYoX4ftbyPqDU86');
+                    assert.equal(address, 'xprv9xzQzPVSHf3pCsetFfoP3v3PyLZPHLqV9JBKLgasEQtCDpdZrSXGDVA1SVcL9H1QSePgRTiDD6D6debCoZfoThJ8oCfBK6DQMMBXpA8DYPM');
                 })
             })
+        
     });
 });
--- test/integration/mnemonicToSignedTransaction_test.js
+++ test/integration/mnemonicToSignedTransaction_test.js
@@ -1,19 +1,20 @@
+
 /*eslint-env node, mocha, es6 */
 process.env.NODE_ENV = 'test';
 const chai = require('chai');
 const assert = chai.assert;
 import Elrn from '..';
 
-suite('mnemonic To Signed, Built transaction', function() {
+suite('mnemonicTo Signed and Built transaction', function() {
     test('can sign and build a transaction from a mnemonic', function() {
         const config = require(__dirname + '/config/options.js');
         const elrnClient = new Elrn(config)
-        let mnemonic = 'strategy worth garlic gallery gesture happy trash boil ceiling weasel happy patrol'
+        const mnemonic = 'strategy worth garlic gallery gesture happy trash boil ceiling weasel happy patrol'
         return elrnClient.mnemonicToSeed(mnemonic)
         .then((seed) => {
             return elrnClient.seedToRoot(seed)
-            .then((root) => {
-                return elrnClient.masterRootToAccountPubKey(root, "m/44'/0'/0'/0/0", 0)
+            .then((rootNode) => {
+                return elrnClient.bip44RootToAccountPubKey(rootNode, "m/44'/0'/0'/0/0", 0)
                 .then((xPub) => {
                     return elrnClient.xPubToActivity(xPub, 'bitcoin')
                     .then((activity) => {
@@ -25,26 +26,26 @@ suite('mnemonic To Signed, Built transaction', function() {
                                 .then((inputs) => {
                                     return elrnClient.inputsToPaths(inputs, xPub)
                                     .then((paths) => {
-                                        return elrnClient.masterRootToAccountPrivKey(root, "m/44'/0'/0'/0/0", 0)
+                                        return elrnClient.bip44RootToAccountPrivKey(rootNode, "m/44'/0'/0'/0/0", 0)
                                         .then((xPriv) => {
                                             return elrnClient.xPrivToKeyPair(xPriv, paths)
                                             .then((keyPairs) => {
-                                                return elrnClient.inputToUnsignedTransaction(inputs, '1FQcZ9AfYAGasWVHXYP7Nd4n2pDSdiwkWy', changeAddress, 1000, 1000)
+                                                return elrnClient.inputsToUnsignedTransaction(inputs, '1FQcZ9AfYAGasWVHXYP7Nd4n2pDSdiwkWy', changeAddress, 1000, 1000)
                                                 .then((unsignedTransaction) => {
                                                     return elrnClient.signTransaction(unsignedTransaction, keyPairs)
                                                     .then((signedTransaction) => {
-                                                        console.log(signTransaction.build().toHex())
-                                                    });
-                                                });
-                                            });
-                                        });  
-                                    }); 
-                                }); 
-                            }); 
-                        });
-                    });          
-                });
-            });
-        });
+                                                        assert.equal(signedTransaction.build().toHex().length , 744 )
+                                                    }).catch(err => console.log(err))
+                                                }).catch(err => console.log(err))
+                                            }).catch(err => console.log(err))
+                                        })  .catch(err => console.log(err))
+                                    }).catch(err => console.log(err)) 
+                                }) .catch(err => console.log(err))
+                            }).catch(err => console.log(err))
+                        }).catch(err => console.log(err))
+                    }).catch(err => console.log(err))       
+                }).catch(err => console.log(err))
+            }).catch(err => console.log(err))
+        }).catch(err => console.log(err))
     });
 });
