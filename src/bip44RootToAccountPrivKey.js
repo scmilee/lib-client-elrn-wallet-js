70ba264176329a80e35e320101b712f4fa0bbfe3
scmilee <drudged@oregonstate.edu>
2018-07-18 10:04:16 -0700

    fix function name inside bip44RootToAccountPrivKey

--- src/bip44RootToAccountPrivKey.js
+++ src/bip44RootToAccountPrivKey.js
@@ -1,5 +1,5 @@
 
-export default function masterRootToAccountPivKey(rootNode, path, accountNumber) {
+export default function bip44RootToAccountPivKey(rootNode, path, accountNumber) {
     return new Promise((resolve, reject) => {
         try {
             const splitPath = path.split('/');
