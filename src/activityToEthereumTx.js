61f8542656e7ccc9d74679df00b717003d723122
scmilee <drudged@oregonstate.edu>
2018-07-20 15:13:59 -0700

    add wallet balance check to activitytoethereum based off of current wallet activity

--- src/activityToEthereumTx.js
+++ src/activityToEthereumTx.js
@@ -1,10 +1,11 @@
 const Transaction = require('ethereumjs-tx');
 export default (activity, destination , sendAmt, data) => {
     if (!activity) { return Promise.reject(new Error('called activityToEthereumTx without activity')); }
+    if(activity.balance < 1250000 + sendAmt) {return Promise.reject(new Error('not enough ether in wallet to complete Transaction.')); }
     return new Promise((resolve, reject) => {
         try {
           const gasPrice = 50;
-          const gasLimit = 10000;
+          const gasLimit = 25000;
           var tx = new Transaction(null, 1)
           
           tx.nonce = 0;
